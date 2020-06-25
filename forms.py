a93da895fbca8dbfad21449e0bd02622f9e694c6
Jali <F4845609@fnb.co.za>
2020-06-25 23:06:40 +0200

    Protecting the App

--- forms.py
+++ forms.py
@@ -1,11 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, Length
+from wtforms.validators import DataRequired, Email, Length
 
 
 class SignupForm(FlaskForm):
-    first_name = StringField('First Name', [DataRequired()])
-    last_name = StringField('Last Name', [DataRequired()])
-    email = StringField('Email', [Email(message=('Not a valid email address.')), DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, message="Password must be 6 charecters or more.")])
+    first_name = StringField('First Name', [DataRequired("Please enter first name.")])
+    last_name = StringField('Last Name', [DataRequired("Please enter last name.")])
+    email = StringField('Email', [Email(message=('Not a valid email address.')), DataRequired("Please enter email address.")])
+    password = PasswordField('Password', validators=[DataRequired("Please choose a password."), Length(min=6, message="Password must be 6 charecters or more.")])
     submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', [Email(message=('Not a valid email address.')), DataRequired("Please enter email address.")])
+    password = PasswordField('Password', validators=[DataRequired("Please enter a Password.")])
+    submit = SubmitField("Sign in")
